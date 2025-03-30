3456be82d692e7a0c8fdab3b29cf9439305e34db
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2025-03-30 13:41:36 -0600

    feat: added new PDF and update README, code, notes, and solutions

--- code/NNLA.jl/test/basic_algos.jl
+++ code/NNLA.jl/test/basic_algos.jl
@@ -1,6 +1,16 @@
 include("../src/NNLA.jl")
 using .NNLA: dot, backsubstitution
+
 @testset "dot product tests" begin
     @test dot(Float64[1.0,0.0,0.0], Float64[0.0, 1.0,0.0]) == 0.0
     @test isapprox(dot(Float64[1.0,1.0,0.0], Float64[1.0, 1.0,0.0]), 2.0) == true
+    @test_throws DimensionMismatch dot(Float64[1.0,0.0,0.0], Float64[0.0, 1.0,0.0, 0.0])
 end
+
+@testset "backsubstitution tests" begin
+    A = [1.0 2.0 3.0; 0.0 1.0 4.0; 0.0 0.0 1.0]
+    b = [1.0, 2.0, 3.0]
+    @test backsubstitution(A, b) ≈ [1.0, 0.0, 3.0]
+end
+
+# @testset "gaussian_elimination! tests" begin end
